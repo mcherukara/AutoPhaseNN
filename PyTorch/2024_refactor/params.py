fa3d5fb3cccef067e2f8f682fde9ae436ace6e70
mcherukara <mathew.j.cherukara@gmail.com>
2024-01-19 23:38:12 -0600

    more streamlining of code in 2024 refactor

--- PyTorch/2024_refactor/params.py
+++ PyTorch/2024_refactor/params.py
@@ -12,7 +12,7 @@ data_path = '../../data/CDI_simulation_upsamp_noise/'
 
 #Training parameters
 EPOCHS = 60 #Full cycle is 12 epochs, good to end on a minimum
-BASE_LR = 1e-4
+BASE_LR = 1e-4 #Scaled by number of GPUs and triangle2 cyclicLR
 INIT_SW = 0.07 #Initial shrink wrap
 FINAL_SW = 0.1  #Final shrink wrap
 CONST_EPOCHS = 0 #How many epochs to not increase SW
